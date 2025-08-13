74c3e9bd8d76bc286d4935026167df371e1b8c2a
Tom Tuddenham <ferrisoxide@gmail.com>
2025-08-13 11:54:02 +0930

    fix for rubocop violations

--- spec/helpers/labels_helper_spec.rb
+++ spec/helpers/labels_helper_spec.rb
@@ -1,20 +1,22 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
 RSpec.describe LabelsHelper, type: :helper do
   describe '#hex_to_rgb' do
-    it 'converts hex color to rgb array' do
+    it 'converts hex color to rgb array' do # rubocop:disable RSpec/MultipleExpectations
       expect(helper.hex_to_rgb('#14b8a6')).to eq([20, 184, 166])
       expect(helper.hex_to_rgb('14b8a6')).to eq([20, 184, 166])
       expect(helper.hex_to_rgb('#000000')).to eq([0, 0, 0])
       expect(helper.hex_to_rgb('ffffff')).to eq([255, 255, 255])
     end
 
-    it 'handles blank input' do
+    it 'handles blank input' do # rubocop:disable RSpec/MultipleExpectations
       expect(helper.hex_to_rgb(nil)).to eq([0, 0, 0])
       expect(helper.hex_to_rgb('')).to eq([0, 0, 0])
     end
 
-    it 'converts color names to rgb' do
+    it 'converts color names to rgb' do # rubocop:disable RSpec/ExampleLength,RSpec/MultipleExpectations
       expect(helper.hex_to_rgb('blue')).to eq([0, 0, 255])
       expect(helper.hex_to_rgb('teal')).to eq([20, 184, 166])
       expect(helper.hex_to_rgb('red')).to eq([255, 0, 0])
@@ -23,7 +25,7 @@ RSpec.describe LabelsHelper, type: :helper do
       expect(helper.hex_to_rgb('grey')).to eq([128, 128, 128])
     end
 
-    it 'is case-insensitive and trims whitespace' do
+    it 'is case-insensitive and trims whitespace' do # rubocop:disable RSpec/MultipleExpectations
       expect(helper.hex_to_rgb('  Blue  ')).to eq([0, 0, 255])
       expect(helper.hex_to_rgb('TEAL')).to eq([20, 184, 166])
     end
