47c6873142181e8f720cfe70109b65df993f86e6
Tom Tuddenham <ferrisoxide@gmail.com>
2025-09-15 17:45:44 +0930

    1257 redesigned free sdg landing page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -23,14 +23,13 @@ module ApplicationHelper
   end
 
   def link_to_registration(link_class: '')
-    params = params&.key?(:theme) ? { theme: current_theme } : {}
     link_class = merge_tailwind_class(
       'text-lg font-semibold leading-6 text-white bg-teal-900 hover:bg-teal-700 px-4 py-2 rounded-sm', link_class
     )
 
     link_to(
       'Get Started',
-      new_user_registration_path(params),
+      new_user_registration_path,
       class: link_class,
       data: { turbo: false }
     )
@@ -49,7 +48,7 @@ module ApplicationHelper
 
   # Render an SVG icon from views/icons
   # Source: https://www.writesoftwarewell.com/how-to-render-svg-icons-in-rails
-  def render_icon(icon, classes: '')
+  def render_icon(icon, classes: 'w-6 h-6')
     render "icons/#{icon}", classes:
   end
 
@@ -77,6 +76,7 @@ module ApplicationHelper
   private
 
   # NOTE: Similar code in LandingPagesHelper#brand_image_path_for_current_theme
+  # TODO: DRY this up, use a convention to locate images based on theme name
   def brand_image_path_for_current_theme
     case current_theme
     when :free_sdg
