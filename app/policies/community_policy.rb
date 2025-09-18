5820f322109e734f036d5a5b721d69c2ca16a793
Tom Tuddenham <ferrisoxide@gmail.com>
2025-09-18 15:22:09 +0930

    out-of-band fix for community policy

--- app/policies/community_policy.rb
+++ app/policies/community_policy.rb
@@ -3,6 +3,6 @@
 # Policy for Community Labels. NB we are phasing these out, so they are turned off by default.
 class CommunityPolicy < LabelPolicy
   def index?
-    current_workspace.community_labels
+    current_workspace&.community_labels
   end
 end
